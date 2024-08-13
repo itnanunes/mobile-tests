f31980a63c4af7cee0b7c74c39a10627a928c91a
Itnã Santana nunes <160541233+itnanunes@users.noreply.github.com>
2024-08-13 19:49:30 -0300

    Update login.test.js

--- test/specs/login.test.js
+++ test/specs/login.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@wdio/globals'
+import { expect, driver } from '@wdio/globals'
 import HomePage from '../pageobjects/home.page.js'
 import LoginPage from '../pageobjects/login.page.js'
 import ProfilePage from '../pageobjects/profile.page.js'
@@ -6,8 +6,8 @@ import ProfilePage from '../pageobjects/profile.page.js'
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await HomePage.openMenu('profile')
-        await LoginPage.login('itna@ebac.com', '123123')
-        await HomePage.openMenu('profile')
+        await LoginPage.login('itna@ebac.com.br', '123123')
+        await ProfilePage.openMenu('profile')
         expect (( await ProfilePage.profileName('santana itna')).isDisplayed()).toBeTruthy()
     })
 })
