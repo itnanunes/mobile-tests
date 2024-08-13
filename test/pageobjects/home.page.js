60afc8465f7175db3a13b169e8a419849865599c
Itnã Santana nunes <160541233+itnanunes@users.noreply.github.com>
2024-08-13 19:48:17 -0300

    Update home.page.js

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -1,12 +1,7 @@
 import { $ } from '@wdio/globals'
-import Page from './page.js';
 
-/**
- * Subpágina contendo seletores e métodos específicos para a página de login
- */
-class HomePage extends Page {
+class HomePage {
     async openMenu(menu) {
-        await $(`id:tab-${menu}`).waitForExist(); // Espera o elemento existir
         await $(`id:tab-${menu}`).click();
     }
 }
