514188b203923e72377b380143be28f50a96e966
Itnã Santana nunes <160541233+itnanunes@users.noreply.github.com>
2024-08-14 15:22:53 -0300

    Update login.page.js

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,23 +1,21 @@
 import { $ } from '@wdio/globals'
 
 class LoginPage {
-    
-    get email () {
-        return $('id:email');
+    get email (){
+        return $('id:email')
+    }
+    get password (){
+        return $('android=new UiSelector().text("Password")')
+    }
+    get btnLogin(){
+        return $('~Login')
     }
 
-    get inputPassword () {
-        return $('android=new UiSelector().text("Password")');
-    }
-
-    get btnLogin () {
-        return $('~Login');
-    }
-
-    async login (email, password) {
-        await this.email.setValue(email);
-        await this.inputPassword.setValue(password);
-        await this.btnLogin.click();
+    async login(email, password){
+        await this.email.setValue(email)
+        await this.password.setValue(password)
+        await this.btnLogin.click()
     }
 }
+
 export default new LoginPage();
